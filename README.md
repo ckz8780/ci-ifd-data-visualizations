945aed2d41de1818e5b09f797bd72ce1c2053205
Chris Zielinski <ckz8780@gmail.com>
2018-05-12 23:10:26 +0000

    Added scaled bar chart examples

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 This repo demonstrates the basic usage of SVG and D3. This is part of the Interactive Frontend Development Module for CodeInstitute.
 
+##### This project has been lightly extended to consolidate code:
+
+- Various SVG and D3 functions have been added to scripts.js
+- jQuery has been utilized to call all functions in sequence on document.ready()
+
 Basic SVG usage: https://ckz8780.github.io/ifd-data-visualizations/svg.html
 
 Basic D3 usage: https://ckz8780.github.io/ifd-data-visualizations/d3.html
--- js/scripts.js
+++ js/scripts.js
@@ -75,8 +75,126 @@ function createBarChart() {
         .attr("fill", "white");
 }
 
+function createBarChartWithScale() {
+    // These height/width measurements represent the height/width of the SVG viewport
+    var w = 500;
+    var h = 500;
+    var barPadding = 1;
+    var dataset = [
+        450, 100, 100, 750, 500, 25, 350, 120, 80, 130,
+        110, 102, 375, 200, 175, 168, 180, 230, 205,
+    ];
+    
+    // Here we set a scale to allow values that expand outside the viewport.
+    var scale = d3.scaleLinear()
+                        .domain([0, d3.max(dataset)])
+                        .range([0, h]);
+                        
+    var colWidth = w/dataset.length;
+    var barWidth = colWidth - barPadding;
+    
+    var svg = d3.select("#d3-scale-barchart")
+        .append("svg")
+        .attr("width", w)
+        .attr("height", h);
+        
+    svg.selectAll("rect")
+        .data(dataset)
+        .enter()
+        .append("rect")
+        .attr('fill', 'purple')
+        .attr("x", function(d, i) {
+            return i * colWidth;
+        })
+        .attr("y", function(d) {
+            return h - scale(d);
+        })
+        .attr("width", barWidth)
+        .attr("height", function(d) {
+            return scale(d);
+        });
+        
+    svg.selectAll("text")
+        .data(dataset)
+        .enter()
+        .append("text")
+        .text(function(d) {
+            return d;
+        })
+        .attr("text-anchor", "middle")
+        .attr("x", function(d, i) {
+            return i * colWidth + barWidth / 2;
+        })
+        .attr("y", function(d) {
+            return h - scale(d) + 14;
+        })
+        .attr("font-family", "sans-serif")
+        .attr("font-size", "11px")
+        .attr("fill", "white");
+}
+
+function createBarChartSquashed() {
+    // These height/width measurements represent the height/width of the SVG viewport
+    var w = 500;
+    var h = 200;
+    var barPadding = 1;
+    var dataset = [
+        450, 100, 100, 750, 500, 25, 350, 120, 80, 130,
+        110, 102, 375, 200, 175, 168, 180, 230, 205,
+    ];
+    
+    // Here we set a scale to allow values that expand outside the viewport.
+    var scale = d3.scaleLinear()
+                        .domain([0, d3.max(dataset)])
+                        .range([0, h]);
+                        
+    var colWidth = w/dataset.length;
+    var barWidth = colWidth - barPadding;
+    
+    var svg = d3.select("#d3-scale-barchart-squashed")
+        .append("svg")
+        .attr("width", w)
+        .attr("height", h);
+        
+    svg.selectAll("rect")
+        .data(dataset)
+        .enter()
+        .append("rect")
+        .attr('fill', 'darkgreen')
+        .attr("x", function(d, i) {
+            return i * colWidth;
+        })
+        .attr("y", function(d) {
+            return h - scale(d);
+        })
+        .attr("width", barWidth)
+        .attr("height", function(d) {
+            return scale(d);
+        });
+        
+    svg.selectAll("text")
+        .data(dataset)
+        .enter()
+        .append("text")
+        .text(function(d) {
+            return d;
+        })
+        .attr("text-anchor", "middle")
+        .attr("x", function(d, i) {
+            return i * colWidth + barWidth / 2;
+        })
+        .attr("y", function(d) {
+            return h - scale(d) + 14;
+        })
+        .attr("font-family", "sans-serif")
+        .attr("font-size", "11px")
+        .attr("fill", "white");
+}
+
 $(document).ready(function() {
     createCircles();
     createBarChart();
+    createBarChartWithScale();
+    createBarChartSquashed();
 });
 
