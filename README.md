abae0eebbb9745ed54806b0d78794be4d8b09733
Chris Zielinski <ckz8780@gmail.com>
2018-05-13 17:44:10 +0000

    added line chart example

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ This repo demonstrates the basic usage of SVG and D3. This is part of the Intera
 
 - Various SVG and D3 functions have been added to scripts.js
 - jQuery has been utilized to call all functions in sequence on document.ready()
+- IMPORTANT: This project uses D3 version 4 which changes some functionality significantly (see https://keithpblog.org/post/upgrading-d3-from-v3-to-v4/)
 
 Basic SVG usage: https://ckz8780.github.io/ifd-data-visualizations/svg.html
 
--- js/dcscripts.js
+++ js/dcscripts.js
@@ -155,9 +155,43 @@ function delayedExternalPieCharts() {
     }
 }
 
+function delayedExternalLineCharts() {
+    queue()
+        .defer(d3.json, "data/transactions.json")
+        .await(makeGraphs);
+        
+    function makeGraphs(error, tdata) {
+        var ndx = crossfilter(tdata);
+        var parseDate = d3.timeParse("%d/%m/%Y");
+        
+        tdata.forEach(function(d){
+            d.date = parseDate(d.date);
+        });
+        
+        var date_dim = ndx.dimension(dc.pluck('date'));
+        var total_spend_per_date = date_dim.group().reduceSum(dc.pluck('spend'));
+        var minDate = date_dim.bottom(1)[0].date;
+        var maxDate = date_dim.top(1)[0].date;
+        
+        dc.lineChart("#spend-per-month")
+            .width(1000)
+            .height(300)
+            .margins({top: 10, right: 50, bottom: 30, left: 50})
+            .dimension(date_dim)
+            .group(total_spend_per_date)
+            .transitionDuration(500)
+            .x(d3.scaleTime().domain([minDate,maxDate]))
+            .xAxisLabel("Month")
+            .yAxis().ticks(4);
+            
+        dc.renderAll();
+    }
+}
+
 $(document).ready(function() {
     transDataBasic();
     transDataSeparated();
     delayedExternalCharts();
     delayedExternalPieCharts();
+    delayedExternalLineCharts();
 });
